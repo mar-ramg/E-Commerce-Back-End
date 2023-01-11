58e679b5c3db725cfc041d1c7dd226799bb0ea27
who cares <mariaaramirez80@gmail.com>
2023-01-11 12:59:18 -0800

    update tag's name by it ID

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -50,3 +50,23 @@ router.get('/:id', (req, res) => {
       });
   });
 
+  router.put('/:id', (req, res) => {
+    // update a tag's name by its `id` value
+    Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbTagData => {
+        if (!dbTagData){
+          res.status(404).json({message:'No tag found with this id'});
+          return;
+        }
+        res.json(dbTagData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
