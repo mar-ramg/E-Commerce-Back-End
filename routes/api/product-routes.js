ebff9122318600cbe8461050ed9e653a11330633
who cares <mariaaramirez80@gmail.com>
2023-01-11 12:52:19 -0800

    create new product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -55,3 +55,32 @@ router.get('/:id', (req, res) => {
         res.status(500).json(err);
       });
   });
+
+router.post('/', (req, res) => {
+    Product.create({
+       product_name: req.body.product_name,
+       price: req.body.price,
+       stock: req.body.stock,
+       category_id: req.body.category_id,
+       tagIds: req.body.tagIds
+       })
+       .then((product) => {
+         // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+         if (req.body.tagIds.length) {
+           const productTagIdArr = req.body.tagIds.map((tag_id) => {
+             return {
+               product_id: product.id,
+               tag_id,
+             };
+           });
+           return ProductTag.bulkCreate(productTagIdArr);
+         }
+         // if no product tags, just respond
+         res.status(200).json(product);
+       })
+       .then((productTagIds) => res.status(200).json(productTagIds))
+       .catch((err) => {
+         console.log(err);
+         res.status(400).json(err);
+       });
+   });
